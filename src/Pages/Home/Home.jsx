24ea9e351f72c0bbac337723a34231bf524f7d92
quarshie133 <wrightsprince@gmail.com>
2024-08-09 13:30:32 +0000

    refactroing

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,21 +7,16 @@ const Home = () => {
   return (
     <main className="flex-grow container mx-auto px-4 py-8">
       <section className="flex flex-col items-center text-center">
-        <video
-          src={heroVideo}
-          autoPlay
-          loop
-          muted
-          className="w-full h-64 object-cover rounded-lg shadow-md"
-        ></video>
-        <h2 className="text-4xl font-semibold text-blue-900 mt-8">
-          Welcome to Horus
-        </h2>
-        <p className="mt-4 text-gray-700 max-w-prose">
+       
+        <h1 className="text-4xl font-semibold text-blue-900 mt-8">
+          Site is under construction
+        </h1>
+        <p>coming soon...</p>
+        {/* <p className="mt-4 text-gray-700 max-w-prose">
           Horus is an educational game designed to teach players about climate
           change and resilience. Explore engaging stories, interactive learning,
           and real-world data to build a better future.
-        </p>
+        </p> */}
         
       </section>
     </main>
